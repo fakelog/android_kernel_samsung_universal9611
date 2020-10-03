1e6f1f09007bd7fe11c427c21e6a0cb0c7573bf5
thirdsty <solodyankin.daniil.go@gmail.com>
2020-10-03 11:18:21 +0500

    move to Linux 1.14.79

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -307,7 +307,6 @@ int bpf_stackmap_copy(struct bpf_map *map, void *key, void *value);
 int bpf_fd_array_map_update_elem(struct bpf_map *map, struct file *map_file,
 				 void *key, void *value, u64 map_flags);
 int bpf_fd_array_map_lookup_elem(struct bpf_map *map, void *key, u32 *value);
-void bpf_fd_array_map_clear(struct bpf_map *map);
 int bpf_fd_htab_map_update_elem(struct bpf_map *map, struct file *map_file,
 				void *key, void *value, u64 map_flags);
 int bpf_fd_htab_map_lookup_elem(struct bpf_map *map, void *key, u32 *value);
